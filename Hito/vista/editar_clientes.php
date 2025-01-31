386c4fdae2d12476a6eddf50d570737e9677917e
IgnacioAriasCampusfp <ignacio.ariastomas24@campusfp.es>
2025-01-31 13:43:09 +0100

    cambios

--- Hito/vista/editar_clientes.php
+++ Hito/vista/editar_clientes.php
@@ -42,7 +42,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <!-- Barra de navegacion para los distintos archivos necesarios -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
-        <a class="navbar-brand" href="../index.php">Hito</a>
+        <a class="navbar-brand" href="../index.php">StreamWeb</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
--- Hito/vista/lista_clientes.php
+++ Hito/vista/lista_clientes.php
@@ -52,7 +52,7 @@ function calcularCosto($cliente, $costos) {
     <!-- Barra de navegacion para los distintos archivos necesarios -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
-        <a class="navbar-brand" href="../index.php">Hito</a>
+        <a class="navbar-brand" href="../index.php">StreamWeb</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
