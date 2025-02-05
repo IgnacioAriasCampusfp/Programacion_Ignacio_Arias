fda4a4023d3a2112a24fe0188a0dafa9f44adb85
IgnacioAriasCampusfp <ignacio.ariastomas24@campusfp.es>
2025-02-05 13:03:18 +0100

    Cambio de alta_usuario
    Modificacion del html para correccion

--- programacion/Proyecto_Login/vista/alta_usuario.php
+++ programacion/Proyecto_Login/vista/alta_usuario.php
@@ -29,12 +29,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
-    <title>Añadir Cliente</title>
+    <title>Añadir Usuario</title>
 </head>
 
 <body>
     <div class="container mt-4">
-        <h1>Añadir Cliente</h1>
+        <h1>Añadir Usuario</h1>
         <form action="alta_usuario.php" method="POST">
             <div class="mb-3">
                 <label for="nombre" class="form-label">Nombre de usuario</label>
